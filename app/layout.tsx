18a1db7f6f713e689dc3da4093d1ccf48cec44b9
shivamrox87 <shivamrox87@gmail.com>
2025-02-22 15:54:12 +0530

    Initial commit: PWM Group website

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from 'next/link';
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -113,6 +114,9 @@ export default function RootLayout({
             </div>
           </div>
         </footer>
+
+        {/* Add Analytics */}
+        <Analytics />
       </body>
     </html>
   );
