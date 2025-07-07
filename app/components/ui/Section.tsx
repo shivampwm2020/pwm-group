0aff43d0ef7f79626f50b5207216e3a555e62aae
pranjalipongde09 <pranjalipongde9@gmail.com>
2025-07-07 13:21:36 +0530

    fix: corrected invalid import in Section.tsx

--- app/components/ui/Section.tsx
+++ app/components/ui/Section.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
-import { cn } from "@/app/lib/blogs";
+
 import { components } from "@/app/styles/design-system";
+import { cn } from "@/lib/utils";
 
 const sectionVariants = cva("relative", {
   variants: {
